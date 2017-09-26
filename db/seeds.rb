2df4c47847d9642072fb30bbbfaa233ae5979ef9
Sean McDonnell <sean@freshpow.io>
2017-09-26 13:14:51 -0400

    solo_app made with rails and bootstrap

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Recipe.create!(title: 'overnight oats', instructions: 'oatmeal, freshpow, blueberries, coconut, chocolate pea milk- let sit in a bowl overnight, enjoy in the morning.')
+Recipe.create!(title: 'pb & j', instructions: 'use daves bread, almond butter, and strawberry spread')
+Recipe.create!(title: 'salad', instructions: 'get fresh produce, wash them, put in a bowl')
+Recipe.create!(title: 'avocado toast', instructions: 'toast bread, cut avocado, put on bread')
+Recipe.create!(title: 'smootie', instructions: 'fresh produce with freshpow in a nutribullet')
